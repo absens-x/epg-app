5d5f5f84d973621b6b3bec6edc34fff77ac83cdb
absens-x <absens.x@gmail.com>
2021-05-23 00:59:17 +0300

    change: structure; create: API server service

---
+++ src/api/EPGServerAPI.js
@@ -0,0 +1,42 @@
+class EPGServerAPI {
+    BASE_API = "http://epg.domru.ru";
+    CITY = ""
+
+    constructor(city = "ekat") {
+        this.CITY = city
+    }
+  
+    getResource = async (url) => {
+      const query = this.BASE_API + url;
+      const res = await fetch(query);
+  
+      if (!res.ok) {
+        throw new Error(`Ошибка при обращении к ресурсу ${query}. Status code: ${res.status}`);
+      }
+
+      return await res.json();
+    };
+  
+    getAllThemes = async () => {
+      const res = await this.getResource("/channeltheme/list");
+      return res;
+    };
+  
+    getAllChannels = async () => {
+        const res = await this.getResource(`/channel/list?domain=${this.CITY}`);
+        return res;
+    };
+
+    getChannelByID = async (id) => {
+        const res = await this.getResource(`/channel/info?chid=${id}&domain=${this.CITY}`)
+        return res
+    }
+  }
+  
+  const obj = new EPGServerAPI();
+
+// obj.getAllThemes().then(res => console.log(res));
+  
+// obj.getAllChannels().then(res => console.log(res))
+
+// obj.getChannelByID(1).then(res => console.log(res));
---
+++ src/containers/App/App.js
@@ -0,0 +1,12 @@
+ 
+import './App.css';
+import "../../api/EPGServerAPI"
+function App() {
+  return (
+    <div className="App">
+       
+    </div>
+  );
+}
+
+export default App;
