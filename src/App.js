eff88c29f657c4caa8fc6ba6b2475e5fa66ff504
absens-x <absens.x@gmail.com>
2021-05-23 19:21:03 +0300

    add: Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
  
-import './App.css';
-import "../../api/EPGServerAPI"
+ 
+import "./sass/_index.scss"
+import "./services/EPGServerAPI"
+
+
 function App() {
   return (
     <div className="App">
-       
+       <Headers/>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+
+import classes from "./Header.module.scss"
+
+const Header = () => {
+    return <div className="">
+        
+    </div>
+}
+
+export default Header
